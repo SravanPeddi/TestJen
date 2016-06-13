9fdc72712bffbeb9408d27bc0ca1ca2e63026dad
Sravan Peddi <sravanpeddi@sravan-macbook-pro.local>
2016-06-13 17:07:38 +0530

    Initial Commit

---
+++ SraJen/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  SraJen
+//
+//  Created by Sravan Peddi on 6/13/16.
+//  Copyright © 2016 Sravan Peddi. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ SraJen/ViewController.m
@@ -0,0 +1,27 @@
+//
+//  ViewController.m
+//  SraJen
+//
+//  Created by Sravan Peddi on 6/13/16.
+//  Copyright © 2016 Sravan Peddi. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
